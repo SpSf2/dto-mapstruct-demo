633469929696e000b6026517151a2cb7429fc561
SpSf2 <kiworks.duglas@gmail.com>
2026-07-15 12:53:10 +0200

    interface User Mapper creada

---
+++ src/main/java/com/example/service/UserMapper.java
@@ -0,0 +1,13 @@
+package com.example.service;
+
+import org.mapstruct.Mapper;
+
+import com.example.dto.UserResponse;
+import com.example.entity.User;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserResponse mapUsertoUserResponse(User user);
+
+}
