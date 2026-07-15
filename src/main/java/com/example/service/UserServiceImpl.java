78594c4e2b7d4d828c02dead370fa22a77a1a43c
SpSf2 <kiworks.duglas@gmail.com>
2026-07-15 14:06:44 +0200

    mapeo mediante map struct

--- src/main/java/com/example/service/UserServiceImpl.java
+++ src/main/java/com/example/service/UserServiceImpl.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 public class UserServiceImpl implements UserService {
 
     private final UserDao userDao;
+    private final UserMapper userMapper;
 
     @Override
     public UserResponse getUserById(long id) {
@@ -25,12 +26,13 @@ public class UserServiceImpl implements UserService {
 
         User user = userDao.findById(id).orElseThrow(() -> new RuntimeException("User not found!!!"));
 
-        userResponse = new UserResponse(
+       /*userResponse = new UserResponse(
             user.getId(), 
             user.getUsername(), 
             user.getPassword(), 
             DateTimeFormatter.ISO_LOCAL_DATE.format(user.getDateOfBirth())//solo para dar formato
-    );
+    );*/
+        userResponse = userMapper.mapUsertoUserResponse(user);
 
 
         return userResponse;
