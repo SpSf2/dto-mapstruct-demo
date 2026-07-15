96347dbc88d62ebff41789791956768b26b54069
SpSf2 <kiworks.duglas@gmail.com>
2026-07-15 10:57:51 +0200

    mapeo manual explicado

---
+++ src/main/java/com/example/service/ContactService.java
@@ -0,0 +1,8 @@
+package com.example.service;
+
+import com.example.entity.Contact;
+
+public interface ContactService {
+
+    Contact getContactById(long id);
+}
---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.service;
+
+import com.example.dto.UserResponse;
+
+public interface UserService {
+
+    UserResponse getUserById(long id);
+}
