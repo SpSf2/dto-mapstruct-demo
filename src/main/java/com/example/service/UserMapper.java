b6fedb825f5e2640777826993ed5bcd1a8fbce37
SpSf2 <kiworks.duglas@gmail.com>
2026-07-15 13:17:58 +0200

    anotaciones @Mapping en el metodo de la interface UserMapper

--- src/main/java/com/example/service/UserMapper.java
+++ src/main/java/com/example/service/UserMapper.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.example.dto.UserResponse;
 import com.example.entity.User;
@@ -8,6 +9,9 @@ import com.example.entity.User;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    
+    @Mapping(target = "name", source = "username") //replace with the actual field name in User
+    @Mapping(target = "dob", source = "dateOfBirth")
     UserResponse mapUsertoUserResponse(User user);
 
 }
